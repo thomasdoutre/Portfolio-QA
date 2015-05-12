caa31de08a267f78fd921558f50caed7e9696ac5
thomasdoutre <thomas.doutre@icloud.com>
2015-05-12 18:48:29 +0200

    TickersSet : Ajout corps méthodes + JavaDoc

--- PortfolioRiskManagement/src/TickersSet.java
+++ PortfolioRiskManagement/src/TickersSet.java
@@ -1,14 +1,31 @@
 
+/**
+ * This class decribes set of tickers
+ * @author  Thomas Doutre
+ * @version 1.0
+ * @since   2015-05-10
+ */
+
 public class TickersSet {
 
+	String[] tickers;
+	
+	/**
+	 * This method returns the number of tickers in the set.
+	 * @return double number of tickers.
+	 */
+	
 	public int getLength() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.tickers.length;
 	}
 
+	/**
+	 * This method returns the ith ticker, in String format.
+	 * @param i no of the ticker to get.
+	 * @return String string associated with the ith ticker.
+	 */
 	public String getTickerString(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.tickers[i];
 	}
 
 }
