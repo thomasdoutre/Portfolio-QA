f122798ef6cfc5bb6b417b54e73acea6547c91d6
thomasdoutre <thomas.doutre@icloud.com>
2015-05-14 01:07:57 +0200

    Exemple d'utilisation fonctionnel

--- PortfolioRiskManagement/src/Main.java
+++ PortfolioRiskManagement/src/Main.java
@@ -4,17 +4,28 @@ import java.util.GregorianCalendar;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		// EXEMPLE DE FORMATION ET MUTATION D'UN PORTEFEUILLE
+		
+		/* On indique les actifs dans lesquels on veut investir et les dates pour la simulation historique */
 		String[] tickers = {"GSPC","AAPL","FCHI","LG.PA","GSZ.PA","KER.PA","RNO.PA","AIR.PA"};
 		Calendar startCalendar = new GregorianCalendar(2014,0,27);
 		Calendar endCalendar = Calendar.getInstance();
 
+		//On récupère les données et on fait les liens avec l'ensemble d'actifs.
 		TickersSet tickersSet = new TickersSet(tickers);
 		Data data = new Data(tickersSet,startCalendar,endCalendar);
 		tickersSet.setData(data);
 		
+		//Création d'un portefeuille avec poids initiaux
 		double[] weights = {0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125};
 		Portfolio portfolio = new Portfolio(tickersSet, weights);
+
+		//Pour la mutation
+		double[] weightsClone = {0,0,0,0,0.25,0.25,0.25,0.25};
+		Portfolio portfolioClone = portfolio.cloneWithDifferentWeights(weightsClone);
+	
+
 		
 		
 	}
