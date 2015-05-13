dc3b48ba4f2ed42740cc8a2d194065fb0bac2bb4
thomasdoutre <thomas.doutre@icloud.com>
2015-05-14 00:25:37 +0200

    Méthode pour construire un portefeuille

--- PortfolioRiskManagement/src/Main.java
+++ PortfolioRiskManagement/src/Main.java
@@ -1,10 +1,22 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		/*String[] tickers = {"GSPC","AAPL","FCHI","LG.PA","GSZ.PA","KER.PA","RNO.PA","AIR.PA"};
-		double[] weights = {0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125};*/
-		//
+		String[] tickers = {"GSPC","AAPL","FCHI","LG.PA","GSZ.PA","KER.PA","RNO.PA","AIR.PA"};
+		Calendar startCalendar = new GregorianCalendar(2014,0,27);
+		Calendar endCalendar = Calendar.getInstance();
+
+		TickersSet tickersSet = new TickersSet(tickers);
+		Data data = new Data(tickersSet,startCalendar,endCalendar);
+		tickersSet.setData(data);
+		
+		double[] weights = {0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125};
+		Portfolio portfolio = new Portfolio(tickersSet, weights);
+		
+		
 	}
 
 }
