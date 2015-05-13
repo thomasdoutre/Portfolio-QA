c954949f3f7d58d1c2aa8fef70e55e8487ae360d
thomasdoutre <thomas.doutre@icloud.com>
2015-05-13 20:38:18 +0200

    Constructeur Portfolio

--- PortfolioRiskManagement/src/Portfolio.java
+++ PortfolioRiskManagement/src/Portfolio.java
@@ -8,6 +8,68 @@
 public class Portfolio {
 	
 	private TickersSet tickersSet;
+	private double[] weights;
+	private double[] returns;
+	private Risk risk;
+	private double expectedReturn;
+	
+	/**
+	 * This method is used to construct a portfolio with all its features.
+	 * Useful for cloning
+	 * @param tickers the tickers
+	 * @param weights the weights
+	 * @param logReturns the log returns
+	 * @param rawReturns the raw returns
+	 * @param valueAtRisk the VaR
+	 * @param conditionalValueAtRisk the CVaR
+	 * @param expectedReturn the expected return
+	 */
+	// ATTENTION : ici on ne clone pas, les références aux objets type double[] sont les mêmes, on clonera
+	// ces pointeurs avant de faire appel à ce constructeur.
+	
+	public Portfolio(TickersSet tickers,double[] weights,double[] returns, Risk risk, double expectedReturn){
+		this.setTickersSet(tickers);
+		this.setWeights(weights);
+		this.setReturns(returns);
+		this.setRisk(risk);
+		this.setExpectedReturn(expectedReturn);
+		
+	}
+
+	/**
+	 * @return the risk
+	 */
+	public Risk getRisk() {
+		return risk;
+	}
+
+	/**
+	 * @param risk the risk to set
+	 */
+	public void setRisk(Risk risk) {
+		this.risk = risk;
+	}
+
+	/**
+	 * @return the expectedReturn
+	 */
+	public double getExpectedReturn() {
+		return expectedReturn;
+	}
+
+	/**
+	 * @param expectedReturn the expectedReturn to set
+	 */
+	public void setExpectedReturn(double expectedReturn) {
+		this.expectedReturn = expectedReturn;
+	}
+
+	/**
+	 * @param returns the returns to set
+	 */
+	public void setReturns(double[] returns) {
+		this.returns = returns;
+	}
 
 	/**
 	 * @return the tickersSet
@@ -23,21 +85,27 @@ public class Portfolio {
 		this.tickersSet = tickersSet;
 	}
 
-
-	public double[] getReturns() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * @return the weights
+	 */
 	public double[] getWeights() {
-		// TODO Auto-generated method stub
-		return null;
+		return weights;
 	}
 
+	/**
+	 * @param weights the weights to set
+	 */
 	public void setWeights(double[] weights) {
-		// TODO Auto-generated method stub
-		
+		this.weights = weights;
 	}
+
+	/**
+	 * @return the returns
+	 */
+	public double[] getReturns() {
+		return returns;
+	}
+
 
 
 }
