33af4c18894ac16eb06b786ba9157a4ef3a85689
thomasdoutre <thomas.doutre@icloud.com>
2015-07-01 00:37:59 +0200

    FinalVersion + JavaDoc

--- PortfolioRiskManagement/src/Optionnel/Tools.java
+++ PortfolioRiskManagement/src/Optionnel/Tools.java
@@ -3,7 +3,7 @@ package Optionnel;
 
 /**
  * This class includes some useful methods
- * @author  Thomas Doutre
+ * @author thomasdoutre
  * @version 1.0
  * @since   2015-05-01
  */
@@ -47,6 +47,23 @@ public class Tools {
 	}
 	
 	/**
+	 * This method is used to display an array of int is the console.
+	 * @param v This is the array to be displayed
+	 */
+
+	public static void printArray(int[] v) {
+		System.out.println();
+		System.out.println("=================================================================");
+		for (int i : v) {
+			System.out.print(i);
+			System.out.print("\t");
+		}
+		System.out.println();
+		System.out.println("=================================================================");
+
+	}
+	
+	/**
 	 * This method is used to clone an array of double.
 	 * @param array the array to be cloned
 	 * @return the cloned array.
@@ -57,6 +74,49 @@ public class Tools {
 			cloned[i]=array[i];
 		}
 		return cloned;
+	}
+	
+	/**
+	 * This method is used to display the sum of double elements in an array.
+	 * @param array the array.
+	 */
+	
+	public static void printSumArray(double[] array){
+		double sum =0;
+		for(int i=0; i<array.length;i++){
+			sum+=array[i];
+		}
+		System.out.println("sumArray = "+sum);
+	}
+	
+	/**
+	 * This method is used to norm an aray in order to get the sum of its elements equal to 1.
+	 * @param array the array.
+	 * @return the resulting normed array.
+	 */
+	public static double[] normArray(double[] array,double total){
+		double sum =0;
+		for(int i=0; i<array.length;i++){
+			sum+=array[i];
+		}
+		System.out.println("sum = "+sum);
+		for(int i=0; i<array.length;i++){
+			array[i]=array[i]*total/sum;
+		}
+		return array;
+	}
+	
+	/**
+	 * This method is used to get the sum of double elements in an array.
+	 * @param array the array.
+	 * @return the resulting sum.
+	 */
+	public static double sumArray(double[] array){
+		double sum =0;
+		for(int i=0; i<array.length;i++){
+			sum+=array[i];
+		}
+		return sum;
 	}
 	
 }
