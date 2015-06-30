7d8e8c55956557188ae92142564cb44a0b573249
thomasdoutre <thomas.doutre@icloud.com>
2015-07-01 00:47:11 +0200

    FinalVersion + JavaDoc

--- PortfolioRiskManagement/src/solver/parametres/FonctionLineaire.java
+++ PortfolioRiskManagement/src/solver/parametres/FonctionLineaire.java
@@ -1,8 +1,6 @@
 package solver.parametres;
 
 /**
- * {@inheritDoc}
- * <p>
  * A chaque appel de modification de la température, la température descend d'un pas constant.
  * Le nombre réel d'itérations est une approximation puisque le pas linéaire est un double é précision limitée.
  */
@@ -23,15 +21,15 @@ public class FonctionLineaire extends Fonction {
 }
 
 	/**
-	 * {@inheritDoc}
 	 * <p>
 	 * Ici, on ajoute le pas é la température et on vérifie si la température résultante est inférieure é la température de fin.
+	 * Pour le recuit quantique, modifierT() modifie Gamma, pas la température, on a donc des valeurs croissantes.
 	 */
 	public boolean modifierT() {
-		if (this.t+this.pasLineaire < this.Tfinal) {
+		if (this.t+this.pasLineaire > this.Tfinal) {
 			return false;
 		} else {
-			this.t += this.pasLineaire; //pas lineaire negatif
+			this.t += this.pasLineaire; //pas lineaire positif
 			return true;
 		}
 	}
