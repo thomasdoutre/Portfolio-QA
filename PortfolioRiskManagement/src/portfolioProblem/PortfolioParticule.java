ac31141ac3e126082e838e7e2f4c4b899f79da3a
thomasdoutre <thomas.doutre@icloud.com>
2015-07-01 00:41:32 +0200

    FinalVersion + JavaDoc

--- PortfolioRiskManagement/src/portfolioProblem/PortfolioParticule.java
+++ PortfolioRiskManagement/src/portfolioProblem/PortfolioParticule.java
@@ -6,14 +6,17 @@ import solver.commun.Etat;
 import solver.commun.IMutation;
 import solver.commun.Probleme;
 
+/**
+ * This class decribes a particle (problem)
+ * @author thomasdoutre
+ * @version 1.0
+ * @since   2015-05-10
+ */
+
 public class PortfolioParticule extends Probleme {
 
 	int Nombrerepliques;
-	
-	
-	
-	
-	
+
 	public PortfolioParticule(EnergiePotentielle Ep, IMutation mutation, EnergieCinetique Ec, int nombrerepliques,TickersSet tickersSet, double[] weights,double[] returns) {
 		this.Ec = Ec;
 		this.mutation = mutation;
@@ -35,7 +38,6 @@ public class PortfolioParticule extends Probleme {
 
 	@Override
 	public void sauvegarderSolution() {
-		// TODO Auto-generated method stub
 		for (Etat portfolio : etats){
 			( (Portfolio) portfolio).sauvegarderSolution();
 		}
