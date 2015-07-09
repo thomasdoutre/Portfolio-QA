412fc4aeb4087f364a7a0a40785f1913f3dee634
thomasdoutre <thomas.doutre@icloud.com>
2015-07-09 16:55:12 +0200

    Create Particle.java

---
+++ SimplePortfolioRiskManagement/Particle.java
@@ -0,0 +1,51 @@
+
+public class Particule {
+
+	public Portfolio[] etatsParticule;
+	
+	public Particule(TickersSet tickersSet, int nbEtats, double R){
+		this.etatsParticule = new Portfolio[nbEtats];
+		for(int i = 0 ; i<nbEtats ; i++){
+			Portfolio portfolio = new Portfolio(tickersSet);
+			portfolio.initialiser(R);
+			this.etatsParticule[i] = portfolio;
+		}
+	}
+	
+	public Particule(int nbEtats){
+		this.etatsParticule = new Portfolio[nbEtats];
+	}
+	
+	public Particule clone(){
+		Particule cloneParticule = new Particule(this.etatsParticule.length);
+		for (int i = 0; i < this.etatsParticule.length; i++) {
+			cloneParticule.etatsParticule[i]=this.etatsParticule[i].clone();
+		}
+		return cloneParticule;
+	}
+	
+	public double computeKinetic1(int indice){
+		
+		double kineticEnergy = 0;
+		for (int i = 0; i < etatsParticule.length; i++) {
+			if(i!=indice){
+				kineticEnergy = kineticEnergy + this.etatsParticule[indice].computeDistance1(this.etatsParticule[i]);
+			}
+		}
+		
+		return kineticEnergy;
+	}
+	
+public double computeKinetic2(int indice){
+		
+		double kineticEnergy = 0;
+		for (int i = 0; i < etatsParticule.length; i++) {
+			if(i!=indice){
+				kineticEnergy = kineticEnergy + this.etatsParticule[indice].computeDistance2(this.etatsParticule[i]);
+			}
+		}
+		
+		return kineticEnergy;
+	}
+	
+}
